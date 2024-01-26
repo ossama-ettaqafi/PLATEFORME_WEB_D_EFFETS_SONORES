a8d511c5f8aaab93ffa36b1d86ed48c0e8a5d559
OSSAMA ETTAQAFI <119759894+imossama@users.noreply.github.com>
2024-01-26 14:30:19 +0100

    main
    trying to link the backend with the frontend.

--- meow-it platform fullstack/frontend/src/app/services/users.service.ts
+++ meow-it platform fullstack/frontend/src/app/services/users.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class UsersService {
-  private apiUrl = 'assets/api/data/users.json';
+  private apiUrl = 'http://127.0.0.1:8000/users';
   private usersIds: string[] = [];
 
   constructor(private http: HttpClient) {
